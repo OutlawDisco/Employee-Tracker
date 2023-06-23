d9926e5c0ce7bc7e91a8820717d177097c9cb2d9
OutlawDisco <aoutlaw00@gmail.com>
2023-06-23 08:31:43 -0600

    added prompts

--- index.js
+++ index.js
@@ -13,4 +13,73 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee_db database.`)
 );
+inquirer.prompt([
+  {
+    type: "list",
+    message: "Select one",
+    name: "main menue",
+    choices: [
+      "view all departments",
+      "view all roles",
+      "view all employees",
+      "add a department",
+      "add a role",
+      "add an employee",
+      "update an employee role",
+    ],
+  },
+]);
 //use db.querey to select department console.table(result)
+db.query("SELECT * FROM department", function (err, results) {
+  console.table(results);
+});
+
+//   {
+//     type: "input",
+//     message: "What is your email address?",
+//     name: "email",
+//   },
+//   {
+//     type: "input",
+//     message: "project name?",
+//     name: "projectName",
+//   },
+//   {
+//     type: "input",
+//     message: "Please write a short description of your project",
+//     name: "description",
+//   },
+//   {
+//     type: "list",
+//     message: "What kind of license should your project have?",
+//     name: "license",
+//     choices: ["MIT", "Apache 2.0", "other"],
+//   },
+//   {
+//     type: "input",
+//     message: "What command should be run to install dependencies?",
+//     default: "npm i",
+//     name: "installation",
+//   },
+//   {
+//     type: "input",
+//     message: "What command should be run to run tests?",
+//     default: "npm test",
+//     name: "tests",
+//   },
+//   {
+//     type: "input",
+//     message: "What does the user need to know about using the repo?",
+//     default: "nothing",
+//     name: "usage",
+//   },
+//   {
+//     type: "input",
+//     message:
+//       "What does the user need to know about contributing to the repo?",
+//     default: "please dont",
+//     name: "contribute",
+//   },
+// ])
+// .then((answers) => {
+//   const mdPageContent = generateMD(answers);
