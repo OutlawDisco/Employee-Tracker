7f5c96ce087abeac663e7e1076d822cb070e2096
OutlawDisco <aoutlaw00@gmail.com>
2023-06-23 09:41:10 -0600

    added dummy data to seeds

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT department (name)
+VALUES ("reception"),
+("salon"),
+("sales"),
+("maintenance");
+
+
+INSERT role(title, salary, department_id)
+VALUES ("manager", 60000, 3),
+("salon_lead", 52000, 2),
+("senior_groomer",60000, 2),
+("groomer", 50000, 2),
+("full_time_receptionist", 35000, 1),
+("part_time_receptionist", 16640, 1),
+('bather', 40000, 2),
+('associate', 3500, 3 ),
+('associate', 30000, 4);
+
+INSERT employee (first_name, last_name, role_id)
+VALUES ('Janessa', 'King', 5),
+('Anna',' Smart', 6),
+('Sabrina', 'King', 3),
+('Rebecca', 'Lynn', 4),
+('Alita', 'Romero', 2),
+('Sharnay', 'Brown', 4),
+('Conner', 'Sullivan', 9),
+('Andrew', 'Li', 8),
+('Derek', 'Span', 7),
+('Maha', 'Chunqiwawa', 7),
+('Keisha', 'Jones', 1);
