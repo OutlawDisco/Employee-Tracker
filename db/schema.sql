9b03dd019b871176651b15d5f2ae865feea317b4
OutlawDisco <aoutlaw00@gmail.com>
2023-06-24 10:12:23 -0600

    added an 's' to employees_db

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 
-USE employee_db;
+USE employees_db;
 
 CREATE TABLE department (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
